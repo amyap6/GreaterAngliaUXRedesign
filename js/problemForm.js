9e7e8924ed475ae9fbcd14e191999bafdf7aaf01
Amy Pryor <amy.a.p@hotmail.com>
2019-12-04 16:26:24 +0000

    ticket booking and problem forms

---
+++ js/problemForm.js
@@ -0,0 +1,13 @@
+function showTrain() {
+    var x = document.getElementById("trainForm");
+    var y = document.getElementById("stationForm");
+    x.style.display = "block";
+    y.style.display = "none";
+}
+
+function showStation() {
+    var x = document.getElementById("stationForm");
+    var y = document.getElementById("trainForm");
+    x.style.display = "block";
+    y.style.display = "none";
+}
